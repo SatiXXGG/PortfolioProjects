9a93128366f7182c2cd20695036f2b636f13b9f8
Alejandro Vela <avelaproyt@gmail.com>
2024-06-03 21:38:37 -0600

    refactor: aria-labels feat: render-history -int-func 😎

--- front-end/calculator/src/App.tsx
+++ front-end/calculator/src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import "./App.css";
 import Button from "./components/button";
+import { evaluate } from "mathjs";
+import { BUTTON_LIST } from "./variables/calculator";
 
 function App() {
   const [result, setResult] = useState("0");
@@ -17,20 +19,30 @@ function App() {
 
     if (regex.test(currentOp)) {
       try {
-        const result: number = eval(currentOp);
+        const result: number = evaluate(currentOp);
         const lastValue = history[history.length - 1];
 
         if (lastValue !== result && lastValue !== currentOp) {
-          setHistory((oldValue) => [...oldValue, currentOp]);
+          setHistory((oldValue: string[]) => [...oldValue, currentOp]);
         }
 
         setResult(result.toString());
         setCurrentOp(result.toString());
       } catch (error) {
-        setResult("Error");
+        setResult("Invalid operation");
       }
     }
   };
+
+  const renderHistoryItem = (item: string, index: number) => (
+    <h2
+      onClick={() => handleHistory(item)}
+      key={index}
+      className="text-2xl text-white text-opacity-50 px-2 py-1 truncate bg-neutral-900 my-1 rounded-md bg-opacity-50 hover:bg-opacity-100 transition-colors duration-200 cursor-pointer customOverflow"
+    >
+      {item}
+    </h2>
+  );
 
   const handleHistory = (value: string) => {
     setCurrentOp(value);
@@ -44,7 +56,7 @@ function App() {
 
   return (
     <main className="flex flex-row h-screen items-center justify-center">
-      <section className="bg-neutral-800 w-80 px-2 py-2 rounded-md h-[25rem]">
+      <section className="bg-neutral-800 w-80 px-2 py-2 rounded-md ">
         <h1 className="text-sm">Calculator</h1>
         <div className="w-[19rem] h-16 bg-neutral-950 py-2 px-1 rounded-md mx-auto mt-2">
           <h2 className="text-2xl justify-center h-[100%] py-1 text-white">
@@ -55,18 +67,11 @@ function App() {
           id="calculator-div"
           className="w-[19rem] mx-auto mt-2 grid grid-cols-4 bg-neutral-950 rounded-md"
         >
-          <Button func={handleButtonClick} title={"1"}></Button>
-          <Button func={handleButtonClick} title={"2"}></Button>
-          <Button func={handleButtonClick} title={"3"}></Button>
-          <Button func={handleButtonClick} title={"+"}></Button>
-          <Button func={handleButtonClick} title={"4"}></Button>
-          <Button func={handleButtonClick} title={"5"}></Button>
-          <Button func={handleButtonClick} title={"6"}></Button>
-          <Button func={handleButtonClick} title={"-"}></Button>
-          <Button func={handleButtonClick} title={"7"}></Button>
-          <Button func={handleButtonClick} title={"8"}></Button>
-          <Button func={handleButtonClick} title={"9"}></Button>
-          <Button func={handleButtonClick} title={"*"}></Button>
+          {BUTTON_LIST.map((character: string, index: number) => {
+            return (
+              <Button key={index} title={character} func={handleButtonClick}></Button>
+            );
+          })}
         </div>
 
         <div className="flex flex-row">
@@ -74,11 +79,13 @@ function App() {
             id="calculate"
             onClick={handleResult}
             className="px-2 py-1 bg-green-700 text-white rounded-sm w-[15rem] h-8 mt-2"
+            aria-label="calculate-result"
           >
             Calculate
           </button>
           <button
             id="calculate"
+            aria-label="Clear"
             onClick={handleDelete}
             className="px-2 py-1 bg-red-700 ml-2 text-white rounded-sm w-[4rem] h-8 mt-2"
           >
@@ -88,18 +95,9 @@ function App() {
       </section>
       <section
         id="history"
-        className="bg-neutral-800 w-80  h-[25rem] ml-5 rounded-md px-2 py-2 overflow-y-auto overflow-x-hidden"
+        className="bg-neutral-800 w-80   ml-5 rounded-md px-2 py-2 overflow-y-auto overflow-x-hidden"
       >
-        {history.map((item) => {
-          return (
-            <h2
-              onClick={() => handleHistory(item)}
-              className="text-2xl text-white text-opacity-50 px-2 py-1 truncate bg-neutral-900 my-1 rounded-md bg-opacity-50 hover:bg-opacity-100 transition-colors duration-200 cursor-pointer customOverflow"
-            >
-              {item}
-            </h2>
-          );
-        })}
+        {history.map(renderHistoryItem)}
       </section>
     </main>
   );
